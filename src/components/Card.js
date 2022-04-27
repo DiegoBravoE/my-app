d1873ef05678cfa8e00523742aa2bfaf11247812
Diego Bravo <dbravo_2015@hotmail.com>
2022-04-27 18:12:11 -0300

    retoques

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,11 @@
 import React from 'react';
 
-
-
 const sobreMi= ({title,item1,item2,item3,background}) => {
-
-
-
     return (
         <div className="card"  id={background} >
 <h2>{title}</h2>
   
-     <ul> 
-     
+     <ul>  
        <li><b>{item1} </b></li>
        <li><b>{item2}</b> </li>
        <li><b>{item3}</b> </li>
