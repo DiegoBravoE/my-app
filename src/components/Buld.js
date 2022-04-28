570c11b09dae888f8e0f385091078d8b59c47d13
Diego Bravo <dbravo_2015@hotmail.com>
2022-04-28 01:53:08 -0300

    comit

--- src/components/Buld.js
+++ src/components/Buld.js
@@ -4,11 +4,11 @@ const Buld = () => {
 
  
     const [isOn ,setIsOn]=useState(false);
-    
+      
     return (
         <div>
-           <div className="off" style={{background:isOn ? " gray":"yellow"}}></div>
-          <button onClick={()=>setIsOn(!isOn)} >on/off</button>  
+          <div  className="off" style={{background:isOn ? "gray":"yellow"}}></div>
+          <button  onClick={() => setIsOn(!isOn)}>on/off</button>  
         </div>
 
     );
