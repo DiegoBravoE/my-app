694134033af7682f221e8d075cff23860e25fd8f
Diego Bravo <dbravo_2015@hotmail.com>
2022-05-03 00:25:20 -0300

    api

--- src/components/Api.js
+++ src/components/Api.js
@@ -14,8 +14,13 @@ const Api = () => {
 console.log(user)
     return (
         <div className="contenedor">
-            <h1>{user.name?.title +" "}{user.name?.first +" "}{user.name?.last}</h1>
+            <h1>{user.name?.title +" "}</h1>
+            <h2>{user.name?.first +" "}{user.name?.last} </h2>
+            <h3>Years:{user.dob?.age}</h3>
             <img src={user.picture?.large} alt="" />
+            <h4>Email:{user.email}</h4>
+            <h3>{user.cell}</h3>
+
         </div>
     );
 };
