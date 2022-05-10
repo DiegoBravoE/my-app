e8aced562317bb193f54a8c475b887c5034ea590
Diego Bravo <dbravo_2015@hotmail.com>
2022-05-09 22:00:51 -0300

    comit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import Counter from './components/Counter';
 import Api2 from './components/Api2';
 import Api from './components/Api';
 import PokemonApi from './components/PokemonApi';
+import NewComponente from './components/NewComponente';
 import './App.css';
 
 function App() {
 
   return (
     <div>
-         <PokemonApi/>
         
+        <NewComponente/>
 
         
     </div>
